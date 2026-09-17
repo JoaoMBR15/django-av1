e6245bfaf6bebf6387a048ecf33dcbd13efd27c9
JoaoMBR15 <joaombrodrigues15@gmail.com>
2026-09-16 21:57:38 -0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def inicio(request):
+    return HttpResponse(
+        'Olá, acervo'
+)
+
+from django.shortcuts import render
+from .models import Livro
+def lista_livros(request):
+    livros = Livro.objects.all() # busca no banco
+    return render(
+        request, 'acervo/lista.html',
+        {'livros': livros} # envia ao template
+)
+
+def novo_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save() # grava no banco
+            return redirect('lista')
+    else:
+        form = LivroForm()
+    return render(request, 'acervo/form.html', {'form': form})
