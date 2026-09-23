9bcbca1b851125cda5fb9991b66cb4e30ed52b1b
JoaoMarcelo <joaombrodrigues15@gmail.com>
2026-09-23 15:01:44 -0300

    lista

--- acervo/views.py
+++ acervo/views.py
@@ -5,3 +5,12 @@ def inicio(request):
     return HttpResponse(
         'Olá, acervo'
 )
+
+from django.shortcuts import render
+from .models import Livro
+def lista_livros(request):
+    livros = Livro.objects.all() # busca no banco
+    return render(
+        request, 'acervo/lista.html',
+        {'livros': livros} # envia ao template
+)
